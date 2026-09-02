56c920ce771c42dd291105cdb3c2fc7cae507b4c
Prerna Mishra <prerna.mishra@lmsathena.com>
2026-09-02 15:31:55 +0530

    youtube link

--- src/components/common/Footer/footer.jsx
+++ src/components/common/Footer/footer.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Send, ShieldCheck, ArrowUpRight, Mail } from 'lucide-react';
 import logoPl from '/public/brand/logo.webp';
+import { label } from 'framer-motion/client';
 
 export default function Footer() {
   return (
@@ -138,6 +139,13 @@ export default function Footer() {
                 color: '#0A66C2',
                 path: 'M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.047c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 110-4.124 2.062 2.062 0 010 4.124zM7.119 20.452H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z',
               },
+              {
+                label: 'Youtube',
+                href: 'https://www.youtube.com/@lifilumen',
+                color: '#FF0000',
+                path: 'M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z',
+              },
+
             ].map(({ label, href, color, path }) => (
               <li key={label}>
                 <a
